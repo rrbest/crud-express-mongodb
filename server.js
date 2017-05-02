f0e1b65346d5a644d640e43adb37d666ed79ed42
Ryan Olsen <ryano1110@gmail.com>
2017-05-01 19:08:12 -0700

    added index.html

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.listen(3000, function() {
 	console.log('listening on 3000')
 })
 
-app.get('/', function(req, res){
-	res.send('Hello world')
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
 })
