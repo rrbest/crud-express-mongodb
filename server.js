0c9f50dac6e9ea7952a85dcf5bfacefe1291aa78
Ryan Olsen <ryano1110@gmail.com>
2017-05-01 19:55:23 -0700

    hooked up mongodb through mlab

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://ryan:dbpassword@ds127731.mlab.com:27731/star-wars-quotes', (err, database) => {
+	if (err) return console.log(err)
+
+	db = database
+
+	app.listen(3000, function() {
+		console.log('listening on 3000')
+	})
+})
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, function() {
-	console.log('listening on 3000')
-})
+
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes', (req, res) => {
-	console.log(req.body)
+	 db.collection('quotes').save(req.body, (err, result) => {
+	 	if (err) return console.log(err)
+
+	 	console.log('saved to database')
+
+	 	res.redirect('/')
+	 })
 })
